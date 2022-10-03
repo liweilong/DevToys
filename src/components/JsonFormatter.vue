5599c7bd277c2aae8fb9c9963fcfa24b8b5d91d6
liweilong.jerry <liweilong.jerry@bytedance.com>
2022-10-03 14:56:00 +0800

    add tabs

---
+++ src/components/JsonFormatter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const orginText = ref("");
+let jsonData = ref(new Object);
+
+</script>
+    
+<template>
+    <n-grid cols="2" >
+        <n-gi>
+            <n-input :value="originText" type="textarea" style="height:35rem" placeholder="请输入Json数据..."/>
+        </n-gi>
+        <n-gi>
+            <json-viewer :value="jsonData" copyable style=""/>
+        </n-gi> 
+    </n-grid>
+</template>
+     
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from "vue";
 import naive from "naive-ui"
+import JsonViewer from "vue-json-viewer"
 import "./style.css";
 import 'vfonts/Lato.css' // 通用字体
 import 'vfonts/FiraCode.css' // 等宽字体
 import App from "./App.vue";
 
-createApp(App).use(naive).mount("#app");
+createApp(App).use(naive).use(JsonViewer).mount("#app");
